bb5e28e87722db61169334da3f0d0154d4525727
ENIKEN1212 <nemadullayevhojiakbar4@gmail.com>
2024-07-19 21:52:31 +0500

    lala

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
 import './App.css';
 import Gallery from './Gallery';
+import { Helmet } from 'react-helmet';
+import Avatar from "./assets/30a2f69bc691c6ca8ac2b2fc4f14b0ae.jpg";
 
 function App() {
   return (
     <div className="App">
-     <Gallery/>
-     <h1>created by ENIKEN</h1>
+      <Helmet>
+        <title> ENIKEN</title>
+       
+      </Helmet>
+      
+      <Gallery />
+      <h1>Created by ENIKEN</h1>
     </div>
   );
 }
