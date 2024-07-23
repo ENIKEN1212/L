e328dec553811a29466db15c9a513bb6e4003f37
ENIKEN1212 <nemadullayevhojiakbar4@gmail.com>
2024-07-23 16:33:03 +0500

    GiggaGallery

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Gallery from './Gallery';
 import { Helmet } from 'react-helmet';
-import Avatar from "./assets/30a2f69bc691c6ca8ac2b2fc4f14b0ae.jpg";
+
 
 function App() {
   return (
